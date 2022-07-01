dc2c4ca4e6e5e81fceaf7739527b0ac31f2014ec
junsj119 <wjstjddud119@naver.com>
2022-07-02 01:19:50 +0900

    zzzxxxccc

--- JenkinsTest/src/main/java/com/hanghae/JenkinsTest/JenkinsTestApplication.java
+++ JenkinsTest/src/main/java/com/hanghae/JenkinsTest/JenkinsTestApplication.java
@@ -9,6 +9,7 @@ public class JenkinsTestApplication {
 	public static void main(String[] args) {
 		System.out.println("제발!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("제발2222222222222222222222222222222");
+		System.out.println("제발3333333333333333333333333333333");
 		SpringApplication.run(JenkinsTestApplication.class, args);
 	}
 
